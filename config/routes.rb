98b006db5a369c5008d6b44ecbd093b987aa141d
Michael <Michael@Sprauer.net>
2015-03-12 22:13:09 +0100

    scaffold session and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get :login, to: 'session#new'
+  post :login, to: 'session#create'
+  delete :login, to: 'session#destroy'
+
   resources :users
 
   root 'users#index'
