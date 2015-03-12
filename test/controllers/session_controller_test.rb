e4268467ff30bf9b56504b1a951dceaaf03f71d7
Michael <Michael@Sprauer.net>
2015-03-12 22:18:58 +0100

    test login page

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -4,6 +4,8 @@ class SessionControllerTest < ActionController::TestCase
   def test_new
     get :new
     assert_response :success
+    assert_not_nil assigns(:user)
+    assert_select 'form.ui.form input', 4
   end
 
   def test_create
