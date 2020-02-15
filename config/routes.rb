4750f1efcb5bf3235a51455a6a68bb7acf1baaf2
Michael Sprauer <Michael.Sprauer@sap.com>
2020-02-15 19:49:01 +0100

    rails g controller session create destroy new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post :login, to: 'session#create'
+  delete :login, to: 'session#destroy'
+  get :login, to: 'session#new'
   resources :users
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
