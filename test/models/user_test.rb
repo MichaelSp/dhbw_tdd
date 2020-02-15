72761caedba28432ae748ecb7ed8bc9d0a3a00c9
Michael Sprauer <Michael.Sprauer@sap.com>
2020-02-15 20:39:54 +0100

    test user has secure password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def user
+    @user ||= User.new
+  end
+
+  def test_invalid
+    assert !user.valid? # password is required!
+  end
+
+  test 'user can store a password' do
+    user = User.new
+    assert user.password_digest.blank?
+    assert_respond_to user, :password=
+    assert_respond_to user, :password
+    user.password = '123'
+    user.save!
+    assert_not user.password_digest.blank?
+  end
+
 end
