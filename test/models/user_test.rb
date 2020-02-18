0fb15046dd52a9047bba8e26810989128f24cd06
Michael Sprauer <Michael.Sprauer@sap.com>
2020-02-18 21:09:52 +0100

    test converage = 100%

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,6 +12,7 @@ class UserTest < ActiveSupport::TestCase
 
   def test_valid
     user.password = 'asodij'
+    user.email = 'valid@mail.de'
     user.valid?
     assert_equal '', user.errors.full_messages.to_sentence
   end
@@ -22,6 +23,7 @@ class UserTest < ActiveSupport::TestCase
     assert_respond_to user, :password=
     assert_respond_to user, :password
     user.password = '123'
+    user.email = 'valid@mail.de'
     user.save!
     assert_not user.password_digest.blank?
   end
